0c1a74d351d4a8ebeae8846c9b6a52cd382d9b29
cafferti <saulkumven@gmail.com>
2023-09-14 00:58:46 +0100

    carousel app set up

--- carousel/src/carousel.jsx
+++ carousel/src/carousel.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css'; // Import your CSS for styling
 
 const CrewCarousel = ({ crewmembers }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? crewmembers.length - 1 : prevIndex - 1));
-  };
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === crewmembers.length - 1 ? 0 : prevIndex + 1));
-  };
-
-  return (
+  useEffect(() => {
+    // This effect runs when currentIndex changes
+  
+    // Initialize an interval
+    const intervalId = setInterval(() => {
+      if (currentIndex >= 0 && currentIndex < crewmembers.length - 1) {
+        // If currentIndex is within bounds, increment it
+        setCurrentIndex(currentIndex + 1);
+      } else if (currentIndex === crewmembers.length - 1) {
+        // If currentIndex is at the last index, reset it to 0
+        setCurrentIndex(0);
+      }
+    }, 3000); // Interval of 3000 milliseconds (3 seconds)
+  
+    // Cleanup function to clear the interval when the component unmounts or when currentIndex changes
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentIndex, crewmembers]); // Include crewmembers as a dependency
+    return (
     <div className="carousel">
-      <button className="carousel-button" onClick={prevSlide}>
-        Previous
-      </button>
       <div className="carousel-content">
         <h3>{crewmembers[currentIndex].Role}</h3>
         <p>{crewmembers[currentIndex].Name}</p>
         <p>{crewmembers[currentIndex].About}</p>
       </div>
-      <button className="carousel-button" onClick={nextSlide}>
-        Next
-      </button>
     </div>
   );
 };
